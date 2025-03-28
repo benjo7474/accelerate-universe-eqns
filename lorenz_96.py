ff328504e37393e68d9746d75dbf10ce93ea6881
Ben Longaker <119361112+benjo7474@users.noreply.github.com>
2025-03-28 12:20:07 -0500

    Add files via upload

--- lorenz_96.py
+++ lorenz_96.py
@@ -3,7 +3,6 @@ import scipy.sparse as sp
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from RK2 import RK2
 
 # These are our constants
 N = 5  # Number of variables
@@ -38,22 +37,14 @@ x0 = F * torch.ones(N, dtype=torch.float32)  # Initial state (equilibrium)
 x0[0] += 0.01  # Add small perturbation to the first variable
 
 # x1 = solve_ivp(L96_2, (0.0,30.0), x0, method='RK45', rtol=1e-6)
-x2 = solve_ivp(L96, (0.0,30.0), x0, method='RK45', rtol=1e-6)
-tvec, xvec = RK2(L96_2, (0.0,30.0), 1e-3, x0)
+soln = solve_ivp(L96, (0.0,30.0), x0, method='RK45', rtol=1e-6)
 
 # Plot the first three variables
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(projection="3d")
-ax1.plot(xvec[0,:], xvec[1,:], xvec[2,:])
+ax1.plot(soln.y[0,:], soln.y[1,:], soln.y[2,:])
 ax1.set_xlabel("$x_1$")
 ax1.set_ylabel("$x_2$")
 ax1.set_zlabel("$x_3$")
 plt.show()
 
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(projection="3d")
-ax2.plot(x2.y[0,:], x2.y[1,:], x2.y[2,:])
-ax2.set_xlabel("$x_1$")
-ax2.set_ylabel("$x_2$")
-ax2.set_zlabel("$x_3$")
-plt.show()
